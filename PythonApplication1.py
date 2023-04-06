aa399765e65d4f073d3e26dd4890ac1f0d0ec65b
bohdan <bohdanstep23@gmail.com>
2023-04-06 11:13:11 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,7 @@
-
+A = input("Введіть рядок: ")
+B = input("Введіть символ: ")
+count = 0
+for char in A:
+    if char == B:
+        count += 1
+print("Кількість входжень символу", B, "в рядок:", count)
