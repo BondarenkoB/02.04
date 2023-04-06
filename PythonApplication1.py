1c1693f8524e6df78590c70c34b46cecb4221bbe
bohdan <bohdanstep23@gmail.com>
2023-04-06 11:10:22 +0300

    done

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,1 +1,11 @@
+s = input("Введіть рядок: ")
+letters = 0
+digits = 0
+for char in s:
+    if char.isalpha():
+        letters += 1
+    elif char.isdigit():
+        digits += 1
+print("Кількість літер:", letters)
+print("Кількість цифр:", digits)
 
